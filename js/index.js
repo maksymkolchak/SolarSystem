a30d4b114a47cac058b3b7e6f460328df7c8104c
Maksym Maksym <maksymkolchak@gmail.com>
2018-10-14 13:49:19 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $("#checker").click(function(){
+$(document).ready(function() {
+    $("#checker").click(function() {
         $("#panel").slideToggle("slow");
     });
 });
@@ -10,37 +10,22 @@ function loadJsonAjax() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-           const marsData = JSON.parse(xhr.responseText);
-           document.querySelector('.mars-checker-status').innerText = marsData.statuss;
-           document.querySelector('.mars-checker-start').innerText = marsData.start;
+            const marsData = JSON.parse(xhr.responseText);
+            document.querySelector('.mars-checker-status').innerText = marsData.statuss;
+            document.querySelector('.mars-checker-start').innerText = marsData.start;
         }
     }
     xhr.open('GET', 'js/trip_data_mars.json', true);
     xhr.send();
 }
 
-// function loadJsonAjax() {
-//     const xhr = new XMLHttpRequest();
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState === 4 && xhr.status === 200) {
-//           const venusData = JSON.parse(xhr.responseText);
-//           document.querySelector('.venus-checker-status').innerText = venusData.statuss;
-//           document.querySelector('.venus-checker-start').innerText = venusData.start;
-//         }
-//     }
-//     xhr.open('GET', 'js/trip_data_venus.json', true);
-//     xhr.send();
-// }
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
 
-// function loadJsonAjax() {
-//     const xhr = new XMLHttpRequest();
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState === 4 && xhr.status === 200) {
-//           const mercuryData = JSON.parse(xhr.responseText);
-//           document.querySelector('.mercury-checker-status').innerText = mercuryData.statuss;
-//           document.querySelector('.mercury-checker-start').innerText = mercuryData.start;
-//         }
-//     }
-//     xhr.open('GET', 'js/trip_data_mercury.json', true);
-//     xhr.send();
-// }
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+}
+);
